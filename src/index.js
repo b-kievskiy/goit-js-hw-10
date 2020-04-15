9ec21d6a695f323cbf896e3d40cf4a8bc8be0741
Bogdan <mormul96@ukr.net>
2020-04-15 22:51:05 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import products from './menu.json';
+import productTmpl from './templates/product.hbs';
+import './templates/theme.js';
 import './styles.css';
+
+const refs = document.querySelector('.js-menu');
+
+buildProductsFeed(products);
+
+function buildProductsFeed(products) {
+  const markup = products.map(product => productTmpl(product)).join('');
+
+  refs.insertAdjacentHTML('beforeend', markup);
+}
